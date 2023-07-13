8b1260b5cb2eaaa21afa72137c5cfd1adf43ebe5
USER <USER@DESKTOP-PFIM1ND>
2023-07-13 23:29:10 +0900

    plus

--- HelloWorld/src/co/yedam/ConditionExe2.java
+++ HelloWorld/src/co/yedam/ConditionExe2.java
@@ -1,32 +1,33 @@
 package co.yedam;
 
+
 import java.util.Scanner;
 
 public class ConditionExe2 {
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
-		while(true) {
-			System.out.println("메뉴를 선택하세요: (숫자를 입력)>> ");
-			System.out.println("1.로그인 2.로그아웃 3.회원가입 4.종료");
-			String menu = scn.nextLine();
 			
-			
-			switch(menu){
-			case 1:
-				login();
-				break;
-			case 2:
-				logout();
-				break;
-			case 3:
-				signUp();
-				break;
-			default:
-				System.out.println("종료");;
-				break;
-			}
-			// switch, if elseif else
-		}
+		
+				while(true) {
+					System.out.println("메뉴를 선택하세요: (숫자를 입력)>> ");
+					String menu = scn.nextLine();
+					System.out.println("1.로그인 2.로그아웃 3.회원가입 4.종료");
+					int a = Integer.parseInt(menu);
+				switch(a){
+				case 1:
+					login();
+					break;
+				case 2:
+					logout();
+					break;
+				case 3:
+					signUp();
+					break;
+				default:
+					if(a==4)
+					break;
+				}
+			}	
 	} // end of main();
 	
 	public static void login() {

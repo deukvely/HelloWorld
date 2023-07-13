36fbdfe406cc0829c85998da68edd42a425e7090
admin <admin@DESKTOP-BD7IGJ8>
2023-07-13 17:47:21 +0900

    all

--- HelloWorld/src/co/yedam/MainExe.java
+++ HelloWorld/src/co/yedam/MainExe.java
@@ -2,10 +2,11 @@ package co.yedam;
 
 public class MainExe {
 	public static void main(String[] args) {
-		System.out.println("Hello, Deukhwa");
+		System.out.println("HELLO, IS THERE ");
 		System.out.println("jdk compile test");
 		
-		PrintExe printExe = new PrintExe(); // 생성자 호출
+		PrintExe printExe = new PrintExe(); //생성자 호출
 		printExe.showInfo();
+		// jnhhjjjjk
 	}
 }
--- HelloWorld/src/co/yedam/OperatorExe.java
+++ HelloWorld/src/co/yedam/OperatorExe.java
@@ -1,30 +1,17 @@
 package co.yedam;
 
-import java.util.Scanner;
-
 public class OperatorExe {
 	public static void main(String[] args) {
-		Scanner sca = new Scanner(System.in);
-		int max = 0;
-		int avg = 0;
-		System.out.println("정수1");
-		String d1 = sca.nextLine();
-		System.out.println("정수2");
-		String d2 = sca.nextLine();
-		System.out.println("정수3");
-		String d3 = sca.nextLine();
+		int sum =0;
+		sum += 10;
 		
-		avg = (Integer.parseInt(d1) + Integer.parseInt(d2) + Integer.parseInt(d3))/3;
-		
-		if(Integer.parseInt(d1)>max) {
-			max = Integer.parseInt(d1);
+		String str= "";
+		if( sum % 2 == 0) {
+			str = "짝수입니다.";
+		}else {
+			str = "홀수입니다.";
 		}
-		if(Integer.parseInt(d2)>max) {
-			max = Integer.parseInt(d2);
-		}
-		if(Integer.parseInt(d3)>max) {
-			max = Integer.parseInt(d3);
-		}
-		System.out.println("최대값은 "+ max+"이고 평균은 "+avg+"입니다.");
+		str = (sum %2 == 0) ? "짝수입니다." : "홀수입니다.";
+		System.out.println(str);
 	}
 }
